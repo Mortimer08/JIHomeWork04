8101480fad772b3e2ec363411988c2cbf8df7ea2
Ilya Makarov <ilyam2003@mail.ru>
2023-02-23 00:18:14 +0500

    Task03 for checking

--- Task03/Task03.java
+++ Task03/Task03.java
@@ -1,4 +1,5 @@
 package Task03;
+
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Random;
@@ -9,18 +10,10 @@ import java.util.Random;
 public class Task03 {
     public static void main(String[] args) {
         LinkedList<Integer> myLinkedList = new LinkedList<Integer>();
-        linkedListRndFill(myLinkedList, 100000);
-        // System.out.println(myLinkedList);
-        long time1 = System.currentTimeMillis();
+        System.out.println("Список:");
+        linkedListRndFill(myLinkedList, 10);
+        System.out.println(myLinkedList);
         System.out.printf("Сумма элементов итератором: %d%n", linkedListSum(myLinkedList));
-        long time2 = System.currentTimeMillis();
-        System.out.printf("Время: %d%n", time2 - time1);
-        System.out.printf("Сумма элементов по индексам: %d%n", linkedListAlterSum(myLinkedList));
-        long time3 = System.currentTimeMillis();
-        System.out.printf("Время: %d%n", time3 - time2);
-        System.out.printf("Сумма элементов через извлечение: %d%n", linkedListAlterSum2(myLinkedList));
-        long time4 = System.currentTimeMillis();
-        System.out.printf("Время: %d%n", time4 - time3);
 
     }
 
@@ -36,21 +29,6 @@ public class Task03 {
         Iterator<Integer> myIterator = myLinkedList.iterator();
         while (myIterator.hasNext()) {
             sum += myIterator.next();
-        }
-        return sum;
-    }
-
-    public static Integer linkedListAlterSum(LinkedList<Integer> myLinkedList) {
-        Integer sum = 0;
-        for (int i = 0; i < myLinkedList.size(); i++) {
-            sum += myLinkedList.get(i);
-        }
-        return sum;
-    }
-    public static Integer linkedListAlterSum2(LinkedList<Integer> myLinkedList) {
-        Integer sum = 0;
-        while (!myLinkedList.isEmpty()) {
-            sum+=myLinkedList.pop();
         }
         return sum;
     }
